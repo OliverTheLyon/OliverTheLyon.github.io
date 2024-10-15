ad505edfaffc5fad514380dfa543ff13576c93b8
Oliver Lyon <ol@olivers-mbp.ucalgary.ca>
2024-10-15 10:51:31 -0600

    added talk locations

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -3,5 +3,15 @@ var addressPoints = [
     "Rouen, France",
     49.4435,
     1.0984
-  ]
+  ],
+  [
+    "Berlin, Germany",
+    52.52,
+    13.405
+  ],
+  [
+    "Boston, USA",
+    42.3601,
+    71.0589
+  ],
 ];
