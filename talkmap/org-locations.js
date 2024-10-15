7fe78e6bd396a2f05295acaf41b44d91faa89bcd
Oliver Lyon <ol@olivers-mbp.ucalgary.ca>
2024-10-15 15:19:16 -0600

    adding poster locations

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,5 +1,15 @@
 var addressPoints = [
   [
+    "Calgary, Canada",
+    51.049999,
+    -114.06666
+  ],
+  [
+    "San Diego, USA",
+    32.715736,
+    -117.161087
+  ],
+  [
     "Rouen, France",
     49.4435,
     1.0984
